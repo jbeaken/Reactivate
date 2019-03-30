41a4731a9dcdb733720d28cb5008d64ed21e9b59
jbeaken <jack747@gmail.com>
2019-03-30 11:26:30 +0000

    Adding fourOfTen test

--- src/test/java/ReactorTests.java
+++ src/test/java/ReactorTests.java
@@ -8,7 +8,7 @@ import java.time.Duration;
 public class ReactorTests {
 
     @Test
-    public void testFlux() {
+    public void testBasicFlux() {
 
         Flux<String> stringFlux = Flux.just("hello", "world");
         Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
@@ -19,8 +19,27 @@ public class ReactorTests {
         merge.subscribe(System.out::println);
     }
 
+    @Test
+    public void testFourOfTenFlux() {
+
+        Flux<Integer> range = Flux.range(0, 10);
+
+        range.subscribe(System.out::println, null, null, subscription -> subscription.request(7));
+    }
+
+
+    @Test
+    public void testFluxWithSubscription() {
+
+        Flux<String> stringFlux = Flux.just("hello", "world");
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
+
+        stringFlux.subscribe(System.out::println, t -> System.err.println(t), () -> {}, subscription -> {subscription.request(2);});
+    }
+
+
     @AfterEach
     public void afterEach() throws InterruptedException {
-        Thread.sleep(20000);
+        Thread.sleep(2000);
     }
 }
