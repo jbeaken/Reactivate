35a870f57a6d6d65e48faa6773c58b75c82f7597
jbeaken <jack747@gmail.com>
2020-04-14 13:12:30 +0000

    A -> D

--- src/test/java/ReactorTests.java
+++ src/test/java/ReactorTests.java
@@ -37,7 +37,7 @@ public class ReactorTests {
 
         Flux merge = Flux.merge(flux1, flux2).log();
 
-        StepVerifier.create(merge).expectNext("A", "1", "B", "D", "E", "1", "2", "3", "4", "5").verifyComplete();
+        StepVerifier.create(merge).expectNext("A", "1", "B", "C", "E", "1", "2", "3", "4", "5").verifyComplete();
     }
 
     @Test
