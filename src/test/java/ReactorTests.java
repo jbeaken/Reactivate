90c89d5947e69b5034573fc279108d474195582e
jbeaken <jack747@gmail.com>
2020-04-18 20:03:52 +0100

    Adding correct sequence

--- src/test/java/ReactorTests.java
+++ src/test/java/ReactorTests.java
@@ -47,7 +47,7 @@ public class ReactorTests {
 
         Flux merge = Flux.concat(flux1, flux2).log();
 
-        StepVerifier.create(merge).expectNext("A", "1", "B", "C", "2", "D", "E", "3", "4", "5").verifyComplete();
+        StepVerifier.create(merge).expectNext("A", "B", "C", "D", "E", "1", "2", "3", "4", "5").verifyComplete();
     }
 
     @Test
