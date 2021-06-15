693bf8dec8d5ee64623cf3db3938100c12853ed9
jbeaken <jack747@gmail.com>
2021-06-15 11:12:15 +0000

    Adding handler

---
+++ webflux/src/main/java/org/mzuri/playground/webflux/sftp/SftpHandler.java
@@ -0,0 +1,32 @@
+package org.mzuri.playground.webflux.sftp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.reactive.function.server.ServerResponse.status;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+class SftpHandler {
+
+    @Autowired
+    private SftpService sftpService;
+
+    public Mono<ServerResponse> upload(ServerRequest request) {
+
+        sftpService.upload();
+
+        return status(OK)
+                .contentType(MediaType.TEXT_PLAIN)
+                .bodyValue("all-good");
+
+    }
+}
