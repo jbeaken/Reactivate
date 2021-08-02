d545d9c95e7613099e3fba439f1c7b8a06d5506d
jbeaken <jack747@gmail.com>
2021-08-02 19:40:25 +0100

    Standalone http server

---
+++ standalone/src/main/java/org/mzuri/playground/reactor/standalone/ReactorServer.java
@@ -0,0 +1,22 @@
+package org.mzuri.playground.reactor.standalone;
+
+import reactor.netty.http.server.HttpServer;
+
+public class ReactorServer {
+
+    public static void main(String[] args) {
+
+        HttpServer.create()   // Prepares an HTTP server ready for configuration
+                .port(0)    // Configures the port number as zero, this will let the system pick up
+                // an ephemeral port when binding the server
+                .route(routes ->
+                        // The server will respond only on POST requests
+                        // where the path starts with /test and then there is path parameter
+                        routes.post("/test/{param}", (request, response) ->
+                                response.sendString(request.receive()
+                                        .asString()
+                                        .map(s -> s + ' ' + request.param("param") + '!')
+                                        .log("http-server"))))
+                .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish its initialization
+    }
+}
