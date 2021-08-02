411b53d49dc7d4cc18f5bf4f2d2d34f274f20d54
jbeaken <jack747@gmail.com>
2021-08-02 20:12:19 +0100

    Http server

--- standalone/src/main/java/org/mzuri/playground/reactor/standalone/ReactorServer.java
+++ standalone/src/main/java/org/mzuri/playground/reactor/standalone/ReactorServer.java
@@ -1,14 +1,16 @@
 package org.mzuri.playground.reactor.standalone;
 
+import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
+
 
 public class ReactorServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        HttpServer.create()   // Prepares an HTTP server ready for configuration
-                .port(0)    // Configures the port number as zero, this will let the system pick up
-                // an ephemeral port when binding the server
+        HttpServer server = HttpServer.create()   // Prepares an HTTP server ready for configuration
+                .port(8080)    // Configures the port number as zero, this will let the system pick up
+                .wiretap(true)
                 .route(routes ->
                         // The server will respond only on POST requests
                         // where the path starts with /test and then there is path parameter
@@ -16,7 +18,11 @@ public class ReactorServer {
                                 response.sendString(request.receive()
                                         .asString()
                                         .map(s -> s + ' ' + request.param("param") + '!')
-                                        .log("http-server"))))
-                .bindNow(); // Starts the server in a blocking fashion, and waits for it to finish its initialization
+                                        .log("http-server"))));
+
+
+        DisposableServer disposableServer = server.bindNow();
+
+        disposableServer.onDispose().block();
     }
 }
