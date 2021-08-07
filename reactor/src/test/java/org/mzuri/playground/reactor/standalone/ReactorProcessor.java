59af04ddd84562dd8752ce7df1d9cd45ac88dea6
jbeaken <j.beaken@elsevier.com>
2021-08-07 09:19:11 +0000

    adding sleep

--- reactor/src/test/java/org/mzuri/playground/reactor/standalone/ReactorProcessor.java
+++ reactor/src/test/java/org/mzuri/playground/reactor/standalone/ReactorProcessor.java
@@ -25,7 +25,7 @@ public class ReactorProcessor {
         Disposable disposable = publish.connect();
 
         // It's multi-thread so wait a sec to receive.
-//        Thread.sleep(5000);
+        Thread.sleep(5000);
 
         disposable.dispose();
     }
