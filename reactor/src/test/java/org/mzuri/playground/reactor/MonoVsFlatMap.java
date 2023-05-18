fa6c7dfc4ab5e353092f3c2a199c74e22b2043b3
jbeaken <jbeaken@elsevier.com>
2023-05-18 19:48:04 +0100

    New test

--- reactor/src/test/java/org/mzuri/playground/reactor/MonoVsFlatMap.java
+++ reactor/src/test/java/org/mzuri/playground/reactor/MonoVsFlatMap.java
@@ -1,15 +1,20 @@
 package org.mzuri.playground.reactor;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MonoVsFlatMap {
 
     @Test
-    void testFlux.() {
+    void testMap() {
         Function<String, String> mapper = String::toUpperCase;
         Flux<String> inFlux = Flux.just("baeldung", ".", "com");
         Flux<String> outFlux = inFlux.map(mapper);
@@ -18,6 +23,19 @@ public class MonoVsFlatMap {
                 .expectNext("BAELDUNG", ".", "COM")
                 .expectComplete()
                 .verify();
+
+    }
+
+    @Test
+    void testFlatMap() {
+        Function<String, Publisher<String>> mapper = s -> Flux.just(s.toUpperCase().split(""));
+
+        Flux<String> inFlux = Flux.just("baeldung", ".", "com");
+        Flux<String> outFlux = inFlux.flatMap(mapper);
+
+        List<String> output = new ArrayList<>();
+        outFlux.subscribe(output::add);
+        assertThat(output).containsExactlyInAnyOrder("B", "A", "E", "L", "D", "U", "N", "G", ".", "C", "O", "M");
 
     }
 }
